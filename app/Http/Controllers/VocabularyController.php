ee7ec4711a509072ca7be1331c9d3e3e683a2715
nayem_mir <mirnayem49@gmail.com>
2020-04-13 08:45:38 +0600

    fixing 123 ....

--- app/Http/Controllers/VocabularyController.php
+++ app/Http/Controllers/VocabularyController.php
@@ -9,20 +9,20 @@ class VocabularyController extends Controller
 {
     public function words()
     {
-        $root_page = HtmlDomParser::file_get_html('https://learnenglish.britishcouncil.org/vocabulary/beginner-to-pre-intermediate',false, null, 0);
+        $root_page = HtmlDomParser::file_get_html("https://learnenglish.britishcouncil.org/vocabulary/beginner-to-pre-intermediate");
         
         $category_array = array();
         foreach($root_page->find('div.views-field.views-field-title') as $categories)
         {
             $catUrl = $categories->children(0)->children(0)->href;
             
-            $category = HtmlDomParser::file_get_html("https://learnenglish.britishcouncil.org/$catUrl",false, null, 0);
+            $category = HtmlDomParser::file_get_html("https://learnenglish.britishcouncil.org/$catUrl");
             
             $find_xmls = $category->find("div.field.field-name-body.field-type-text-with-summary.field-label-hidden div.field-items div.field-item.even",0);
             
             $allxml =  $find_xmls->children(0)->children(0)->href;
             
-            $words_xml = HtmlDomParser::file_get_html($allxml,false, null, 0);
+            $words_xml = HtmlDomParser::file_get_html($allxml);
             
             $category_name = $category->find('h1#page-title.page__title.title',0)->innertext;     
             
@@ -30,7 +30,8 @@ class VocabularyController extends Controller
            
             $words = array();
             
-            foreach ($words_xml->find('SimpleQuestionItem') as $answer){
+            foreach ($words_xml->find('SimpleQuestionItem') as $answer)
+            {
             
                 $word = array();
                 $word["image"]="https:".$answer->children(1)->children(1)->innertext;
@@ -53,7 +54,7 @@ class VocabularyController extends Controller
     
     public function categories()
     {
-        $categories = HtmlDomParser::file_get_html('https://learnenglish.britishcouncil.org/vocabulary/beginner-to-pre-intermediate',true, null, 0);
+        $categories = HtmlDomParser::file_get_html("https://learnenglish.britishcouncil.org/vocabulary/beginner-to-pre-intermediate");
         
         return view('categories',compact('categories'));
         
@@ -63,7 +64,7 @@ class VocabularyController extends Controller
     public function words_by_category($cat)
 
     {
-        $root_page = HtmlDomParser::file_get_html('https://learnenglish.britishcouncil.org/vocabulary/beginner-to-pre-intermediate',false, null, 0);
+        $root_page = HtmlDomParser::file_get_html("https://learnenglish.britishcouncil.org/vocabulary/beginner-to-pre-intermediate");
         
         $category_array = [];
         foreach($root_page->find('div.views-field.views-field-title') as $categories)
@@ -71,22 +72,23 @@ class VocabularyController extends Controller
             $catUrl = $categories->children(0)->children(0)->href;
             if($catUrl == "/vocabulary/beginner-to-pre-intermediate/$cat")
             {
-            $category = HtmlDomParser::file_get_html("https://learnenglish.britishcouncil.org/$catUrl",false, null, 0);
+            $category = HtmlDomParser::file_get_html("https://learnenglish.britishcouncil.org/$catUrl");
             
             $find_xmls = $category->find("div.field.field-name-body.field-type-text-with-summary.field-label-hidden div.field-items div.field-item.even",0);
             
             $allxml =  $find_xmls->children(0)->children(0)->href;
             
-            $words_xml = HtmlDomParser::file_get_html($allxml,false, null, 0);
+            $words_xml = HtmlDomParser::file_get_html($allxml);
             
             $category_name = $category->find('h1#page-title.page__title.title',0)->innertext;     
             $category_name = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $category_name)));
 
             
-        //    if($category_name == $cat){
+        
             $words = [];
             
-            foreach ($words_xml->find('SimpleQuestionItem') as $answer){
+            foreach ($words_xml->find('SimpleQuestionItem') as $answer)
+            {
             
                 $word = array();
                 $word["image"]="https:".$answer->children(1)->children(1)->innertext;
@@ -99,7 +101,7 @@ class VocabularyController extends Controller
          
         }
       
-        }
+    }
 
 
          return view('words_by_category' ,compact('category_array'));
